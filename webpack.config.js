1b4ec004124fd57156d98bcca08d55fabf5a5318
Samson1484 <samson1484@users.noreply.github.com>
2019-09-29 14:24:50 +0800

    01

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path');
+const MiniCssExtractPlugin = require ('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,   // /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+/*
+                use: [
+                    {
+                        loader: 'ss-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+*/
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ],
+    devServer: {
+        open: true,
+        contentBase: path.join(__dirname, '.'),
+        compress: true,
+        port: 9000
+        }
+};
